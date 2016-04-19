453bafe53c868ffe8eb9ba913407397503a18e8d
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-19 13:00:58 -0500

    Initial Commit .. config set up

--- app/modules/gallery/config.js
+++ app/modules/gallery/config.js
@@ -1,4 +1,10 @@
 function config($stateProvider) {
+  $stateProvider
+    .state("gallery", {
+      url:"/",
+      controller:"GalleryController as galleryCtrl",
+      template: require('./view.html')
+    })
 }
 
 export default config;
